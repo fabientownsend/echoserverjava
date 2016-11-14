8aa4264d0cb5186c0fd54de0abaac27c640a2ffc
fabien townsend <fabientownsend@gmail.com>
2016-11-14 17:45:16 +0000

    can launch the echoserver with the main

---
+++ src/main/java/com/echoserver/Main.java
@@ -0,0 +1,14 @@
+package com.echoserver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args){
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter output = new PrintWriter(System.out, false);
+        EchoServer echoServer = new EchoServer(input, output);
+        echoServer.run();
+    }
+}
