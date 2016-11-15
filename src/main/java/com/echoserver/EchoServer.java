129130266e5e017f0a49461a511f5b65276ba384
fabien townsend <fabientownsend@gmail.com>
2016-11-15 11:21:07 +0000

    refactoring public interface

--- src/main/java/com/echoserver/EchoServer.java
+++ src/main/java/com/echoserver/EchoServer.java
@@ -1,35 +1,51 @@
 package com.echoserver;
 
-import org.omg.DynamicAny.DynAnyPackage.InvalidValue;
-
 import java.io.*;
 
 public class EchoServer {
     private final BufferedReader input;
     private final PrintWriter output;
+    private final String EXIT = "exit";
+    private final String CTRLCMESSAGE = "ctrl handler";
 
     public EchoServer(BufferedReader input, PrintWriter output) {
         this.input = input;
         this.output = output;
     }
 
-    public void write(String input) {
+    public void run() {
+        ctrlCHandler();
+
+        try {
+            displayInput();
+        } catch (IOException e) {
+            write(e.getMessage());
+        }
+    }
+
+    private void ctrlCHandler() {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                write(CTRLCMESSAGE);
+            }
+        }));
+    }
+
+    private void displayInput() throws IOException {
+        String value = read();
+
+        while (!value.equals(EXIT)) {
+            write(value);
+            value = read();
+        }
+    }
+
+    private void write(String input) {
         output.println(input);
     }
 
-    public String read() throws IOException {
+    private String read() throws IOException {
         return input.readLine();
-    }
-
-    public void run() {
-        try {
-            String value = read();
-
-            while (!value.equals("exit")) {
-                write(value);
-                value = read();
-            }
-        } catch (Exception e) {
-        }
     }
 }
--- src/test/java/com/echoserver/test/EchoServerTest.java
+++ src/test/java/com/echoserver/test/EchoServerTest.java
@@ -1,7 +1,6 @@
 package com.echoserver.test;
 
 import com.echoserver.EchoServer;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -10,31 +9,27 @@ public class EchoServerTest {
     private EchoServer server = null;
     private StringWriter out = null;
 
-    @Before
-    public void initialize() {
-        final String text = "Hello\nWorld\nexit\n";
-        BufferedReader input = new BufferedReader(new StringReader(text));
+    @Test
+    public void exitTheEchoServerWithoutOutput() throws IOException {
+        initialiseEcheServerWithInput("exit\n");
+        server.run();
 
+        assert out.toString().equals("");
+    }
+
+    @Test
+    public void displayInputUntilExit() throws IOException {
+        initialiseEcheServerWithInput("Hello\nWorld\nexit\n");
+        server.run();
+
+        assert out.toString().equals("Hello\nWorld\n");
+    }
+
+    private void initialiseEcheServerWithInput(String text) {
+        BufferedReader input = new BufferedReader(new StringReader(text));
         out = new StringWriter();
         PrintWriter output = new PrintWriter(out, true);
+
         server = new EchoServer(input, output);
-    }
-
-    @Test
-    public void getTheInputFromTheEchoServer() throws IOException {
-        assert server.read().equals("Hello");
-        assert server.read().equals("World");
-    }
-
-    @Test
-    public void getThePrintedValueInThenEchoServer() throws IOException {
-        server.write("Hello");
-        assert out.toString().equals("Hello\n");
-    }
-
-    @Test
-    public void existTheEchoServerWhenInputIsExit() throws IOException {
-        server.run();
-        assert out.toString().equals("Hello\nWorld\n");
     }
 }
