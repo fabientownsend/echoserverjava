d60d4534d0890c71acf1829e63a4c16ee902be9e
fabien townsend <fabientownsend@gmail.com>
2016-11-15 09:39:12 +0000

    display what the user typed

--- src/main/java/com/echoserver/Main.java
+++ src/main/java/com/echoserver/Main.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args){
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter output = new PrintWriter(System.out, false);
+        PrintWriter output = new PrintWriter(System.out, true);
         EchoServer echoServer = new EchoServer(input, output);
         echoServer.run();
     }
